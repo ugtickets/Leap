e9685627ddb1a14e62da35bb7e420a5207a6e0b7
Yvonne Kusiima <yvonnekusiima00@gmail.com>
2022-05-08 12:30:07 +0300

    africanbookshtml

--- publishers/assets/js/bookearning.js
+++ publishers/assets/js/bookearning.js
@@ -7,19 +7,19 @@ function bookearning() {
     var bookprice = document.getElementById('bookprice').value;
 
     if (royaltyplan == "70% and 65% - $2 to $4.15") {
-        var bookearning = Math.round(70 / 100 * bookprice);
+        var bookearning = 70 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
     if (royaltyplan == "55% and 51% - $4.16 to 5.49") {
-        var bookearning = Math.round(55 / 100 * bookprice);
+        var bookearning = 55 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
     if (royaltyplan == "45% and 42% - $5.50 to $6") {
-        var bookearning = Math.round(45 / 100 * bookprice);
+        var bookearning = 45 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
     if (royaltyplan == "35% and 33% - $0.99 to $200") {
-        var bookearning = Math.round(35 / 100 * bookprice);
+        var bookearning = 35 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
 }
