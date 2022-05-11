934dbb17539983d1782eb90c75c1afbc292fd6cf
Yvonne Kusiima <yvonnekusiima00@gmail.com>
2022-05-12 02:24:58 +0300

    africanbookshtml

--- admin/assets/js/bookearning.js
+++ admin/assets/js/bookearning.js
@@ -2,23 +2,28 @@ $("#royaltyplan, #bookprice").keyup(function () {
     bookearning();
 });
 
-function bookearning() {
+function bookearning() 
+{
     var royaltyplan = document.getElementById('royaltyplan').value;
     var bookprice = document.getElementById('bookprice').value;
 
-    if (royaltyplan == "70% and 65% - $2 to $4.15") {
+    if(royaltyplan == "Ujamaa (Wealth Building Plan - 70%)") 
+	{
         var bookearning = 70 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
-    if (royaltyplan == "55% and 51% - $4.16 to 5.49") {
+    if(royaltyplan == "Uhuru (Freedom Plan - 55%)") 
+	{
         var bookearning = 55 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
-    if (royaltyplan == "45% and 42% - $5.50 to $6") {
+    if(royaltyplan == "Umoja (Unity Plan - 45%)") 
+	{
         var bookearning = 45 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
-    if (royaltyplan == "35% and 33% - $0.99 to $200") {
+    if(royaltyplan == "Ubuntu (Equality Plan- 35%)") 
+	{
         var bookearning = 35 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
