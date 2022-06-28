68e016f66ea9d4e73c21b8848c5e2c2bb37ee0f2
Yvonne Kusiima <yvonnekusiima00@gmail.com>
2022-06-28 21:57:26 +0300

    africanbookshtml

--- authors/assets/js/bookearning.js
+++ authors/assets/js/bookearning.js
@@ -1,28 +1,28 @@
-$("#royaltyplan, #bookprice").keyup(function () {
+$("#paymentplan, #bookprice").keyup(function () {
     bookearning();
 });
 
 function bookearning() 
 {
-    var royaltyplan = document.getElementById('royaltyplan').value;
+    var paymentplan = document.getElementById('paymentplan').value;
     var bookprice = document.getElementById('bookprice').value;
 
-    if(royaltyplan == "Ujamaa (Wealth Building Plan - 70%) $2 to $4.15") 
+    if(paymentplan == "Ujamaa (Wealth Building Plan - 70%) $2 to $4.15") 
 	{
         var bookearning = 70 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
-    if(royaltyplan == "Uhuru (Freedom Plan - 55%) $4.16 to 5.49") 
+    if(paymentplan == "Uhuru (Freedom Plan - 55%) $4.16 to 5.49") 
 	{
         var bookearning = 55 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
-    if(royaltyplan == "Umoja (Unity Plan - 45%) $5.50 to $6") 
+    if(paymentplan == "Umoja (Unity Plan - 45%) $5.50 to $6") 
 	{
         var bookearning = 45 / 100 * bookprice;
         $("#bookearning").val(bookearning);
     }
-    if(royaltyplan == "Ubuntu (Equality Plan- 35%) $0.99 to $200") 
+    if(paymentplan == "Ubuntu (Equality Plan- 35%) $0.99 to $200") 
 	{
         var bookearning = 35 / 100 * bookprice;
         $("#bookearning").val(bookearning);
