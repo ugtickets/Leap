cc03d9b9dccff648efe2979304cef17401ec980b
Yvonne Kusiima <yvonnekusiima00@gmail.com>
2023-05-02 03:37:29 +0300

    africanbooks

--- readers/assets/js/currencyconversion.js
+++ readers/assets/js/currencyconversion.js
@@ -50,4 +50,156 @@ $(document).ready(function() {
 	$.each($('span[id="bookprice"]'), function(index, value) {
 		console.log($(value).text());
 	});
+	
+	$.each($('.amount'), function(priceindex, value) {
+		console.log($(value).text());
+		$.get('https://api.fastforex.io/convert?to=UGX' + '&amount=' + $(value).text() + '&api_key=156518279b-6826dfd2fb-rf9umz', function (response) {
+			var productprice = document.getElementsByClassName("product-price");
+			$(response.result['UGX']).each(function (index, value) {
+				console.log("UGX " + value.toLocaleString());
+				productprice[priceindex].innerHTML = "UGX " + value.toLocaleString();
+			})
+		})
+		.catch(function(error){
+			console.log(error);
+		});		
+	});
+
+	$(".amount").each(function (priceindex, value) {
+		console.log(priceindex.value);
+		$.get('https://api.fastforex.io/convert?to=UGX' + '&amount=' + this.value + '&api_key=156518279b-6826dfd2fb-rf9umz', function (response) {
+			var productprice = document.getElementsByClassName("product-price");
+			$(response.result['UGX']).each(function (index, value) {
+				productprice[priceindex].innerHTML = "UGX " + value.toLocaleString();
+			})
+		})
+		.catch(function(error){
+			console.log(error);
+		});		
+	});
+});
+
+$(document).ready(function() {
+	$("#currencycode").on('change', function () {
+		currencyconversion();
+	});
+
+	var amounts = [];
+	$('.amount').each(function (index, value) {
+		amounts.push($(value).text());
+		
+	});
+	console.log(amounts);
+
+	function currencyconversion()
+	{
+		var currencycode = $("#currencycode").val();
+
+		if(currencycode == "UGX") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('UGX');
+			});
+
+			$(amounts).each(function (index, amount) {
+				console.log(amount);
+				var amountindex = document.getElementsByClassName("amount");
+				$('.amount').each(function () {
+					amountindex[index].innerHTML = (amount * 3750).toLocaleString();
+					//$(value).text((amount * 3750).toLocaleString());
+				})
+				
+			});
+		}
+		else if(currencycode == "KES") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('KES');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 135).toLocaleString());
+			});
+		}
+		else if(currencycode == "TZS") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('TZS');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 2351).toLocaleString());
+			});
+		}
+		else if(currencycode == "RWF") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('RWF');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 1110.8).toLocaleString());
+			});
+		}
+		else if(currencycode == "ZMW") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('ZMW');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 17.32).toLocaleString());
+			});
+		}
+		else if(currencycode == "GHS") 
+		{		
+			$('.currency').each(function (index, value) {
+				$(value).text('GHS');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 11.75).toLocaleString());
+			});
+		}
+		else if(currencycode == "NGN") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('NGN');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 461.51).toLocaleString());
+			});
+		}
+		else if(currencycode == "XAF") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('XAF');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 597.93).toLocaleString());
+			});
+		}
+		else if(currencycode == "XOF") 
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('XOF');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text(($(value).text() * 597.93).toLocaleString());
+			});
+		}
+		else
+		{
+			$('.currency').each(function (index, value) {
+				$(value).text('$');
+			});
+
+			$('.amount').each(function (index, value) {
+				$(value).text($(value).text());
+			});
+		}
+	}
 });
